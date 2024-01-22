9840a302ea96aee071312cb93c5bafaab71326a2
Steve Lund <lund5000@gmail.com>
2024-01-21 20:58:10 -0500

    Refactor into class and add unit tests

--- recorder/record.py
+++ recorder/record.py
@@ -7,82 +7,98 @@ import math
 import os 
 import requests
 import secrets
+import sys
 import time
 
-STREAM_URL = os.getenv('STREAM_URL')
-BUCKET_NAME = os.getenv('BUCKET_NAME')
-CALL_SIGN = os.getenv('CALL_SIGN')
+class Recorder():
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--start', dest='start', help='hour of the day the shift starts (0-23)', type=int, required=True)
-parser.add_argument('--duration', dest='duration', help='duration to record (seconds)', type=int, required=True)
-args = parser.parse_args()
+    last_progress = 0
+    STREAM_URL = os.getenv('STREAM_URL')
+    BUCKET_NAME = os.getenv('BUCKET_NAME')
+    CALL_SIGN = os.getenv('CALL_SIGN')
+    MAX_ATTEMPTS = 3
+    MAX_ATTEMPTS_CODE = 'Exceeded maximum attempts'
 
-def setup_logging():
-    logging.basicConfig(level=logging.DEBUG)
-    if os.getenv('LOGGING') == 'GC':
-        client = google.cloud.logging.Client()
-        client.setup_logging()
+    def __init__(self, mode = 'normal', test_param = None):
+       self.parse_args(mode, test_param)
+       self.setup_logging()
 
-def get_filename():     
-    now = datetime.now()
-    weekday = now.strftime('%w')
-    date_string = now.strftime('%Y-%m-%d')
-    # add a random token to make it harder to scrape the files
-    token = secrets.token_urlsafe(4)
-    return f'{CALL_SIGN}-{weekday}-{args.start}-{date_string}-{token}.mp3'
+    def parse_args(self, mode, test_param):
+        if mode == 'unittest':
+            if test_param is None:
+                sys.exit('Missing test params')
 
-def open_blob(filename):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(filename)
-    return blob.open('wb')
+            self.start = test_param['start']
+            self.duration = test_param['duration']
+        else:
+            parser = argparse.ArgumentParser()
+            parser.add_argument('--start', dest='start', help='hour of the day the shift starts (0-23)', type=int, required=True)
+            parser.add_argument('--duration', dest='duration', help='duration to record (seconds)', type=int, required=True)
+            args = parser.parse_args()
+            self.start = args.start
+            self.duration = args.duration 
 
-last_progress = 0
+    def setup_logging(self):
+        logging.basicConfig(level=logging.DEBUG)
+        if os.getenv('LOGGING') == 'GC':
+            client = google.cloud.logging.Client()
+            client.setup_logging()
 
-def log_progress(filename, elapsed_time):
-    global last_progress
-    progress = min(round(elapsed_time / args.duration * 100), 100)
-    if math.fmod(progress, 5) == 0 and progress > last_progress:
-        logging.info(f'{filename} - {progress}% complete')
-        last_progress = progress
+    def get_filename(self):     
+        now = datetime.now()
+        weekday = now.strftime('%w')
+        date_string = now.strftime('%Y-%m-%d')
+        # add a random token to make it harder to scrape the files
+        token = secrets.token_urlsafe(4)
+        return f'{self.CALL_SIGN}-{weekday}-{self.start}-{date_string}-{token}.mp3'
 
-def save_streamed_response():
-    start_time = time.time()
-    filename = get_filename()
-    file = open_blob(filename)
-    chunk_size = 512 * 1024 # 0.5MB
-    retries = 0
-    max_retries = 3
-    recording = True
+    def open_blob(self, filename):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.BUCKET_NAME)
+        blob = bucket.blob(filename)
+        return blob.open('wb')
 
-    while recording:
-        try:
-            logging.info(f'Attempt #{retries + 1}')
-            with requests.get(STREAM_URL, stream=True) as response:
-                response.raise_for_status()
-                logging.info(f'{filename} - starting ...')                 
-                for chunk in response.iter_content(chunk_size=chunk_size):
-                    if chunk:
-                        file.write(chunk)
-                    else:
-                        logging.info('No chunk to write')
-                    
-                    elapsed_time = time.time() - start_time
-                    log_progress(filename, elapsed_time)         
-                    if elapsed_time >= args.duration:
-                        recording = False
-                        break
-        except Exception:
-            logging.exception('Exception occurred')
-            if retries < max_retries:
-                retries += 1
-                time.sleep(retries)
-                continue
-            else:
-                recording = False
-                break
+    def log_progress(self, filename, elapsed_time):
+        progress = min(round(elapsed_time / self.duration * 100), 100)
+        if math.fmod(progress, 5) == 0 and progress > self.last_progress:
+            logging.info(f'{filename} - {progress}% complete')
+            self.last_progress = progress
+
+    def save_streamed_response(self):
+        start_time = time.time()
+        filename = self.get_filename()
+        file = self.open_blob(filename)
+        chunk_size = 512 * 1024 # 0.5MB
+        attempts = 1
+        recording = True
+
+        while recording:
+            try:
+                logging.info(f'Attempt #{attempts}')
+                with requests.get(self.STREAM_URL, stream=True) as response:
+                    response.raise_for_status()
+                    logging.info(f'{filename} - starting ...')                       
+                    for chunk in response.iter_content(chunk_size=chunk_size):
+                        if chunk:
+                            file.write(chunk)
+                        else:
+                            logging.info('No chunk to write')
+                        
+                        elapsed_time = time.time() - start_time
+                        self.log_progress(filename, elapsed_time)         
+                        if elapsed_time >= self.duration:
+                            recording = False
+                            break
+            except Exception:                
+                logging.exception('Exception occurred')
+                attempts += 1
+                if attempts <= self.MAX_ATTEMPTS:                    
+                    time.sleep(attempts)
+                    continue
+                else:
+                    recording = False
+                    sys.exit(self.MAX_ATTEMPTS_CODE)
 
 if __name__ == '__main__':    
-    setup_logging()
-    save_streamed_response()
+    r = Recorder()
+    r.save_streamed_response()
---
+++ recorder/tests/record_test.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from record import Recorder
+from random import randbytes
+from requests import Response
+
+class recorderTests(unittest.TestCase):
+    def setUp(self):
+        mode = 'unittest'
+        test_param = {
+            'start': 1,
+            'duration': 1
+        }     
+        self.recorder = Recorder(mode, test_param)
+        self.recorder.open_blob = MagicMock()
+        self.mock_time_sleep = patch('time.sleep')
+
+    def infinite_stream(self, chunk_size, decode_content):
+        while True:
+            yield randbytes(chunk_size)   
+
+    def interrupted_stream(self, chunk_size, decode_content):
+        n = 0
+        while True:
+            if n == 2:
+                raise ConnectionResetError
+            else:
+                yield randbytes(chunk_size)
+                n += 1
+
+    def get_response(self, status_code):
+        mock_response = Response()
+        mock_response.raw = MagicMock()
+        mock_response.raw.stream = self.infinite_stream
+        mock_response.status_code = status_code 
+        return mock_response   
+
+    @patch('requests.get')
+    def test_requests_stream(self, mock_get):
+        mock_response = self.get_response(200)        
+        mock_get.return_value = mock_response
+
+        self.recorder.save_streamed_response()        
+        mock_get.assert_called_with(self.recorder.STREAM_URL, stream=True)
+
+    @patch('requests.get')
+    def test_max_attempts(self, mock_get):
+        mock_get.return_value = self.get_response(429)      
+        
+        with self.assertRaises(SystemExit) as context:
+            self.recorder.save_streamed_response()   
+        self.assertEqual(mock_get.call_count, self.recorder.MAX_ATTEMPTS)         
+        self.assertEqual(context.exception.code, self.recorder.MAX_ATTEMPTS_CODE)
+
+    @patch('requests.get')
+    def test_retries_on_reset(self, mock_get):
+        mock_response = self.get_response(200)
+        mock_response.raw.stream = self.interrupted_stream
+        mock_get.return_value = mock_response
+
+        self.recorder.save_streamed_response()
+        self.assertEqual(mock_get.call_count, 2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
