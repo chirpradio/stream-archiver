6155cbd5b0ed3a49fcc536110a1420f5549281e2
Steve Lund <lund5000@gmail.com>
2024-04-17 11:15:02 -0400

    Start VM earlier

--- recorder/create-schedule.sh
+++ recorder/create-schedule.sh
@@ -1,5 +1,5 @@
-gcloud compute resource-policies create instance-schedule restart-stream-recorder \    
+gcloud compute resource-policies create instance-schedule restart-stream-recorder \
   --region=us-central1 \
-  --vm-start-schedule="45 5 * * *" \
+  --vm-start-schedule="30 5 * * *" \
   --vm-stop-schedule="15 3 * * *" \
   --timezone="America/Chicago"
