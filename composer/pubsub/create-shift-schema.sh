82f25aa00565e90aa8b4be62503240bee4e25768
Steve Lund <lund5000@gmail.com>
2024-04-17 21:22:35 -0400

    Fix gcloud create scripts

--- composer/pubsub/create-shift-schema.sh
+++ composer/pubsub/create-shift-schema.sh
@@ -1,7 +1,7 @@
 gcloud pubsub schemas create shift \
   --type=AVRO \
   --definition='{
-    "type" : "record", "name" : "shift"
+    "type" : "record", "name" : "shift",
     "fields" : [
       {"name" : "weekday", "type" : "int"},
       {"name" : "hours", "type": { "type": "array", "items": "int"}}
--- recorder/create-instance.sh
+++ recorder/create-instance.sh
@@ -5,7 +5,7 @@ gcloud compute instances create stream-archive-recorder \
   --image-family=debian-12 \
   --zone=us-central1-a \
   --machine-type=e2-micro \
-  --resource-policies-restart-stream-recorder \
+  --resource-policies=restart-stream-recorder \
   --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write \
   --metadata-from-file=startup-script=./startup.sh \
   --metadata=BUCKET_NAME=${bucket}
